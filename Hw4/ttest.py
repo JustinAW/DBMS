db9637ab36004ae7a04177ab314fe40c39143062
Justin Weigle <jw6858@cs.ship.edu>
2020-03-29 16:11:09 -0400

    ttest calculator made, and data changed to csv

---
+++ Hw4/ttest.py
@@ -0,0 +1,53 @@
+import statistics as stats
+import math
+import csv
+
+def get_avg (times):
+    n = 20
+    return sum(times) / n
+
+
+def get_delta (p, np, stddevp, stddevnp):
+    n = 20
+    top = (n * stddevp**2) + (n * stddevnp**2)
+    bot = (n - 1) + (n - 1)
+    sqrt1 = math.sqrt(top / bot)
+    sqrt2 = math.sqrt((1 / n) + (1 / n))
+    return sqrt1 * sqrt2
+
+
+def get_t (delta, avgp, avgnp):
+    return abs(avgp - avgnp) / delta
+
+
+if __name__=="__main__":
+    p = list()
+    np = list()
+    with open('primtime.csv', 'r') as csvfile:
+        f = csv.reader(csvfile, delimiter='\r')
+        f.__next__() #skip header
+        for row in f:
+            p.append(int(row[0]))
+        csvfile.close()
+    with open('nonprimtime.csv', 'r') as csvfile:
+        f = csv.reader(csvfile, delimiter='\r')
+        f.__next__() #skip header
+        for row in f:
+            np.append(int(row[0]))
+        csvfile.close()
+
+    avgp = get_avg(p)
+    avgnp = get_avg(np)
+    print("Average primary times:\n", avgp)
+    print("Average non primary times:\n", avgnp, "\n")
+
+    stddevp = stats.stdev(p)
+    stddevnp = stats.stdev(np)
+    print("Std dev primary times:\n", stddevp)
+    print("Std dev non primary times:\n", stddevnp, "\n")
+
+    delta = get_delta(p, np, stddevp, stddevnp)
+    print("Delta_xx primary and non primary times:\n", delta, "\n")
+
+    t = get_t (delta, avgp, avgnp)
+    print("T value:\n", t)
